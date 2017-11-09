37a23e22179efefbb5ec17808ba2322532839da2
Spencer Florence <spencer@florence.io>
2017-11-09 09:00:43 -0600

    provide #%top-interaction so that drracket knows the REPL is available

--- lang/empty.rkt
+++ lang/empty.rkt
@@ -1,4 +1,5 @@
 #lang racket/base
+(provide #%top-interaction)
 (require "lang.rkt")
 ;; Since `compile-program` generates syntax that already
 ;; includes binding, avoid exporting anything from the
