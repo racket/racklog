96d024ecb2a24e9774deacb19e867856902dbc78
Jay McCarthy <jay.mccarthy@gmail.com>
2018-05-30 18:26:29 -0400

    Report from Marek Materzok
    (cherry picked from commit a38b6d00afac89db3d57b87d39a818049d2c4c69)

---
+++ tests/pr/pr4.rkt
@@ -0,0 +1,14 @@
+#lang racket/base
+(require racklog
+         tests/eli-tester)
+
+(module+ test
+  (test
+   (%which (x) %true)
+   => '((x . _))
+   
+   (%which (x) (%or (%not (%= x 1)) %true))
+   => '((x . _))
+   
+   (%which (x) (%or (%cut-delimiter (%or (%and (%= x 1) ! %fail) %true)) %true))
+   => '((x . _))))
