7c7dd6a76a3d24da4b043cf97d615ee29eab58f4
Jay McCarthy <jay.mccarthy@gmail.com>
2018-05-30 18:30:59 -0400

    Testing each step
    (cherry picked from commit 5bf9081275e1910750d51849701672fe22a88baf)

--- tests/pr/pr4.rkt
+++ tests/pr/pr4.rkt
@@ -9,6 +9,9 @@
    
    (%which (x) (%or (%not (%= x 1)) %true))
    => '((x . _))
-   
+
+   (%which (x) (%or (%if-then-else (%= x 1) %fail %true) %true))
+   => '((x . _))
+
    (%which (x) (%or (%cut-delimiter (%or (%and (%= x 1) ! %fail) %true)) %true))
    => '((x . _))))
